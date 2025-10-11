8696374e7f7bf783927ef75218c896559ed467b2
ShoheiKondo <runs57061@gmail.com>
2025-10-11 21:46:09 +0900

    ex26とotherの作成

---
+++ ex26/Parent4_26.java
@@ -0,0 +1,9 @@
+package javaSilver.ex26;
+
+
+public class Parent4_26 {
+	int num = 10;
+	// protected staticを付けると実行できる
+	// protected static int num = 10;
+
+}
---
+++ other/Child4_26.java
@@ -0,0 +1,9 @@
+package javaSilver.other;
+import javaSilver.ex26.Parent4_26;
+
+public class Child4_26 extends Parent4_26 {
+    public static void main(String[] args) { 
+        System.out.println(num);       
+    }
+}
+
